6e6d41d7e0fe53027d215194897e93335877bffd
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2020-02-04 10:20:48 +0000

    Initial commit

---
+++ HolidayHelperApp/main.cpp
@@ -0,0 +1,5 @@
+
+int main(int argC, char** argV)
+{
+
+}
---
+++ HolidayHelperTest/HolidayHelperTest.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace HolidayHelperTest
+{
+	TEST_CLASS(HolidayHelperTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}

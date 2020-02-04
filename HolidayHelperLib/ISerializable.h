e3373f7d651b1b5b94c28f4d1ba2f44a196119f1
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2020-02-04 10:33:59 +0000

    Updated language spec to c++/17 Added ISerializable as a test

---
+++ HolidayHelperLib/ISerializable.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <iostream>
+
+namespace HolidayHelper::Utils::Serialization
+{
+
+	//Abstract interface for serializing objects to disk
+	class ISerializable
+	{
+	public:
+		//Pure virtual functions that have to be overloaded in derived classes
+		virtual std::ostream& Serialize(std::ostream& os) = 0;
+		virtual std::istream& Deserialize(std::istream& is) = 0;
+
+		//Out stream operator, allows operator based stream interaction for ISerializabe objects
+		friend std::ostream& operator <<(std::ostream& os, ISerializable& outObj)
+		{
+			//This will call the ISerializable::Serialize as defined in a derived object
+			return outObj.Serialize(os);
+		}
+
+		//In stream operator, allows operator based stream interaction for ISerializabe objects
+		friend std::istream& operator >>(std::istream& is, ISerializable& inObj)
+		{
+			//This will call the ISerializable::Deserialize as defined in a derived object
+			return inObj.Deserialize(is);
+		}
+
+	protected:
+
+		//Helper function for properly reading in a string from istream
+		inline std::string InStreamToString(std::istream& in)
+		{
+			std::streampos prevPos = in.tellg();
+			char curr;
+			int strCnt = 0;
+			do
+			{
+				strCnt++;
+				in.get(curr);
+			} while (curr != '\0');
+			in.seekg(prevPos);
+			char* streamString = new char[strCnt];
+			in.read(streamString, strCnt);
+			return std::string(streamString);
+		}
+
+	};
+}
